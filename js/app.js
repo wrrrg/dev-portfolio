aee2154664524682fcae5e32c6594292da162124
wrrrg <bill.gryta@gmail.com>
2018-03-28 12:02:56 -0500

    arrows sort of working

--- js/app.js
+++ js/app.js
@@ -1,3 +1,51 @@
+// slideshow controls
+
+$(document).ready(function() {
+  $('.project-div').hide();
+  $('#back-arrow').hide();
+  $('#project1').show();
+  $('#project1').addClass('selected');
+});
+
+$(document).on('click', '#forward-arrow', function(){
+  var check = $('.selected').attr('id');
+  $("#back-arrow").show();
+
+  if(check === 'project4'){
+    $('#forward-arrow').hide()
+  }else{
+      $('.project-div').hide();
+      var oldDiv = $('.selected');
+      var newDiv = $('.selected').next('.project-div');
+
+      oldDiv.removeClass('selected');
+      newDiv.addClass('selected');
+
+      newDiv.show();
+  };
+});
+
+$(document).on('click', '#back-arrow', function(){
+    var check = $('.selected').attr('id');
+    $("#forward-arrow").show();
+
+  if(check === 'project1'){
+    $('#back-arrow').hide()
+  }else{
+      $('.project-div').hide();
+      var oldDiv = $('.selected');
+      var newDiv = $('.selected').prev('.project-div');
+
+      oldDiv.removeClass('selected');
+      newDiv.addClass('selected');
+
+      newDiv.show();
+  };
+
+});
+
+
+
 // close landing modal
 
 $(document).on('click', '.modal-landing', function(){
